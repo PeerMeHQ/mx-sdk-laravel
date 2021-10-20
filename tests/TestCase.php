953a248e762cad2f5a0ee4b5c924fa950e202a47
micha vie <micha@freeiam.com>
2021-10-20 15:46:22 +0200

    fix tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,7 +9,7 @@ class TestCase extends Orchestra
 {
     public function fakeApiRequestWithResponse(string $endpoint, string $responseFile): void
     {
-        $response = file_get_contents(__DIR__ . '/api/responses/' . $responseFile);
+        $response = file_get_contents(__DIR__ . '/Api/responses/' . $responseFile);
 
         Http::fake([
             'api.elrond.com/' . ltrim($endpoint, '/') => Http::response($response),
