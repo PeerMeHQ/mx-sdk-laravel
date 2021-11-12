e9200757c2f8a99aa77d9f674e1b71bb37ed5bc0
micha vie <micha@freeiam.com>
2021-11-12 13:12:52 +0100

    add formatter util

---
+++ src/Utils/FormatterUtil.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Superciety\ElrondSdk\Utils;
+
+use Carbon\Carbon;
+
+class FormatterUtil
+{
+    public static function timeToHumanReadable(?Carbon $time): string
+    {
+        if ($time === null) {
+            return null;
+        }
+
+        return now()->addMonths(6)->greaterThan($time)
+            ? $time->shortRelativeToNowDiffForHumans()
+            : $time->format('j F Y');
+    }
+
+
+    public static function numberToHumanReadableShort(int $num): string
+    {
+        // Taken from: https://stackoverflow.com/questions/4116499/php-count-round-thousand-to-a-k-style-count-like-facebook-share-twitter-bu/36365553
+        if ($num > 1000) {
+            $x = round($num);
+            $x_number_format = number_format($x);
+            $x_array = explode(',', $x_number_format);
+            $x_parts = array('k', 'm', 'b', 't');
+            $x_count_parts = count($x_array) - 1;
+            $x_display = $x;
+            $x_display = $x_array[0] . ((int) $x_array[1][0] !== 0 ? '.' . $x_array[1][0] : '');
+            $x_display .= $x_parts[$x_count_parts - 1];
+            return $x_display;
+        }
+
+        return $num;
+    }
+}
