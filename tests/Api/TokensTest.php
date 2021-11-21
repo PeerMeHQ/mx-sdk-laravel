7f6e6ccafb91ec15198734ded038868109e1d46a
micha vie <micha@freeiam.com>
2021-11-22 00:16:05 +0100

    refactor tests minor

--- tests/Api/TokensTest.php
+++ tests/Api/TokensTest.php
@@ -3,7 +3,7 @@
 use Superciety\ElrondSdk\Elrond;
 
 it('gets the transaction history of an NFT ', function () {
-    fakeApiRequestWithResponse('/tokens/QUACK-f01e02-0259/transactions', 'transactions/nft-history.json');
+    fakeApiRequestWithResponse('/tokens/QUACK-f01e02-0259/transactions', 'tokens/transactions.json');
 
     $actual = Elrond::api()
         ->tokens()
