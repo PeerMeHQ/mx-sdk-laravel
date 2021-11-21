373683fe3b42bf8e86fad7ca14081daeca40cc72
micha vie <micha@freeiam.com>
2021-11-21 15:36:52 +0100

    add timeout util

---
+++ src/Utils/Timeout.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Superciety\ElrondSdk\Utils;
+
+use Carbon\Carbon;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Cache;
+
+class Timeout
+{
+    public static function for(string $uniqueAction, Carbon $endsAt, callable $action): bool
+    {
+        $safeCacheKey = 'timeout:'.Str::slug($uniqueAction);
+
+        if (Cache::has($safeCacheKey)) {
+            return false;
+        }
+
+        $action();
+
+        Cache::put($safeCacheKey, true, $endsAt);
+
+        return true;
+    }
+}
