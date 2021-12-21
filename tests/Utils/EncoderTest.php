f2f1282d0aed5a921eefa6f446a2b3b435daf086
micha vie <micha@freeiam.com>
2021-12-21 14:30:40 +0100

    fix tests naming

--- tests/Utils/EncoderTest.php
+++ tests/Utils/EncoderTest.php
@@ -2,8 +2,8 @@
 
 use Superciety\ElrondSdk\Utils\Encoder;
 
-it('toHex it converts strings to hex', fn () => expect(Encoder::toHex('anything'))->toBe('616e797468696e67'));
+it('toHex - converts strings to hex', fn () => expect(Encoder::toHex('anything'))->toBe('616e797468696e67'));
 
-it('toHex it converts single-byte ints to hex', fn () => expect(Encoder::toHex(2))->toBe('02'));
+it('toHex - converts single-byte ints to hex', fn () => expect(Encoder::toHex(2))->toBe('02'));
 
-it('toHex it converts multi-byte ints to hex', fn () => expect(Encoder::toHex(257))->toBe('0101'));
+it('toHex - converts multi-byte ints to hex', fn () => expect(Encoder::toHex(257))->toBe('0101'));
