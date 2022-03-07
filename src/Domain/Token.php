a65c728dcac07d8cba6be1d957aacf02c0b585c4
micha vie <micha@freeiam.com>
2022-03-07 18:19:05 +0100

    update testnet super token id

--- src/Domain/Token.php
+++ src/Domain/Token.php
@@ -7,7 +7,7 @@ use Exception;
 final class Token
 {
     const SuperTokenId = 'SUPER-507aa6';
-    const SuperTokenIdTestnet = 'XSUPER-4abae3';
+    const SuperTokenIdTestnet = 'XSUPER-da8349';
     const SuperTokenIdDevnet = 'XSUPER-5ef129';
 
     public function __construct(
