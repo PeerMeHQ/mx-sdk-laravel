428a2c3023797db0d71218741c8955d2195f1bc5
micha vie <micha@freeiam.com>
2022-06-15 12:04:01 +0200

    update encoder

--- src/Utils/Encoder.php
+++ src/Utils/Encoder.php
@@ -6,7 +6,7 @@ use Superciety\ElrondSdk\Domain\Address;
 
 class Encoder
 {
-    public static function toHex(string|int $value): string
+    public static function toHex(string|int $value, bool $skipHexBytePadding = false): string
     {
         if (is_string($value)) {
             return str_starts_with($value, 'erd1')
@@ -14,14 +14,13 @@ class Encoder
                 : bin2hex(trim($value));
         }
 
-        return static::numberToPaddedHex($value);
+        return $skipHexBytePadding
+            ? dechex($value)
+            : static::toPaddedHex(dechex($value));
     }
 
-    public static function numberToPaddedHex(int $value): string
+    public static function toPaddedHex(string|int $value): string
     {
-        $padding = '0';
-        $hex = dechex($value);
-
-        return strlen($hex) % 2 === 1 ? $padding . $hex : $hex;
+        return strlen($value) % 2 === 1 ? '0' . $value : $value;
     }
 }
