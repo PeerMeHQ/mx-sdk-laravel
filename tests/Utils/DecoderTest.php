5016403a148a09cbba47aff7460278db4b77a0eb
micha vie <micha@freeiam.com>
2022-07-22 10:32:57 +0200

    add decoder test

--- tests/Utils/DecoderTest.php
+++ tests/Utils/DecoderTest.php
@@ -12,3 +12,9 @@ it('fromBase64BigUint - decodes base64 encoded integers', fn ($base64, $val) => 
     ->with([
         ['LBKkOIqYlAAA', '813000000000000000000'],
     ]);
+
+it('bchexdec - decodes a hex encoded bignumber', fn ($hex, $val) => expect(Decoder::bchexdec($hex))->toBe($val))
+    ->with([
+        ['015af1d78b58c40000', '25000000000000000000'],
+        ['1569b9e733474c0000', '395000000000000000000'],
+    ]);
