6d983c0d1b4d017a42de1f4124a5d78921beae67
micha vie <micha@freeiam.com>
2022-08-10 17:26:41 +0200

    add elrond address rule

---
+++ src/Rules/ElrondAddressRule.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Superciety\ElrondSdk\Rules;
+
+use Illuminate\Contracts\Validation\InvokableRule;
+
+class ElrondAddressRule implements InvokableRule
+{
+    const Regex = '/erd1[a-z0-9]{58}/';
+
+    /**
+     * Run the validation rule.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     * @return void
+     */
+    public function __invoke($attribute, $value, $fail)
+    {
+        if (! preg_match_all(self::Regex, $value)) {
+            $fail('The :attribute must be a valid Elrond address.');
+        }
+    }
+}
